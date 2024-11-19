73d6a712c46e31520d592ab82e35a7e7038d01a5
woodart8 <kibeom1145@gmail.com>
2024-11-19 11:51:21 +0900

    Add: 기본 시맨틱 태그 컴포넌트 구현

--- inflow/eslint.config.js
+++ inflow/eslint.config.js
@@ -1,19 +1,24 @@
-import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import js from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+import prettierConfig from '@vue/eslint-config-prettier';
 
 export default [
+  // 파일 지정 및 무시 규칙
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
+    files: ['**/*.{js,mjs,jsx,vue}'], // JavaScript와 Vue 파일만 검사
   },
-
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'], // dist 및 coverage 폴더 무시
   },
 
+  // ESLint 기본 추천 규칙
   js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-  skipFormatting,
-]
+
+  // Vue 플러그인 규칙 (필요에 따라 essential, strongly-recommended, recommended 선택 가능)
+  ...pluginVue.configs['flat/recommended'],
+
+  // Prettier와 충돌하는 규칙 비활성화
+  prettierConfig,
+];
