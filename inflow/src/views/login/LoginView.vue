3a5ee75ffbd689af391fc2cd333105ec0376fdf5
woodart8 <kibeom1145@gmail.com>
2024-11-25 15:54:53 +0900

    Feat: 로그인 에러 처리

--- inflow/src/views/login/LoginView.vue
+++ inflow/src/views/login/LoginView.vue
@@ -4,8 +4,8 @@
     <LoginSection
       v-model:id="employeeNumber"
       v-model:password="password"
-      :invalid="true"
-      :message="'사번 또는 비밀번호가 잘못 되었습니다.'"
+      :invalid="invalid"
+      :message="errorMessage"
       @login="handleLogin"
     />
   </div>
@@ -19,13 +19,20 @@ import axios from 'axios';
 
 const employeeNumber = ref('');
 const password = ref('');
+const invalid = ref(false);
+const errorMessage = ref('');
 
 const handleLogin = async () => {
-  const response = (await axios.post('http://localhost:5000/api/login', {
-    employee_number: employeeNumber.value,
-    password: password.value
-  })).data;
-  console.log(response);
+  try {
+    const response = (await axios.post('http://localhost:5000/api/login', {
+      employee_number: employeeNumber.value,
+      password: password.value
+    })).data;
+  } catch (error) {
+    invalid.value = true;
+    errorMessage.value = error.response.data.error ? error.response.data.error.message : null;
+    console.log(error.response.data);
+  }
 }
 
 
