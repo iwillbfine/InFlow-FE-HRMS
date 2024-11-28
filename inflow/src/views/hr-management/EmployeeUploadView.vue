9721132a29fcb7b224ed887ede49d3d806a8f590
iwillbfine <iwillbfine@naver.com>
2024-11-28 12:00:33 +0900

    사원 경력 정보 등록 구현

--- inflow/src/views/hr-management/EmployeeUploadView.vue
+++ inflow/src/views/hr-management/EmployeeUploadView.vue
@@ -11,7 +11,7 @@
       <MainItem h="calc(100% - 12rem)" w="100%">
         <CommonWidget :cur="1" :list="menuList">
           <FlexItem class="widget-content" h="100%" w="100%"> 
-            <SubMenuNav :cur="subIdx" :list="subMenuList" @clicked="handleClicked" class="ca"/>
+            <SubMenuNav :cur="subIdx" :list="subMenuList" @clicked="handleClicked"/>
             <CommonArticle :label="`${title}`" w="90%">
               <router-view :key="$route.fullPath" />
             </CommonArticle>
@@ -97,9 +97,5 @@ const subMenuList = ref([
   align-items: center;
   padding: 2rem;
   overflow: auto;
-}
-
-.ca {
-  cursor: pointer;
 }
 </style>
