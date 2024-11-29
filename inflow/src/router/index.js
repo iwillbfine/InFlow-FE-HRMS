1376bb0d7a0c3766f934c561522ca29b9b940c46
Haegwan-Choe <kkhagwan@naver.com>
2024-11-30 03:32:29 +0900

    중간 커밋

--- inflow/src/router/index.js
+++ inflow/src/router/index.js
@@ -238,7 +238,7 @@ const routes = [
         component: SalaryDetailView,
       },
       {
-        path: 'list',
+        path: 'list/:employeeId',
         name: 'hr-basic-salary-list',
         component: SalaryListView,
       },
--- inflow/src/views/hr-basic/salary/SalaryListView.vue
+++ inflow/src/views/hr-basic/salary/SalaryListView.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>
-
-  </div>
+  <FlexItem class="content-body" fld="column" h="calc(100% - 6rem)" w="90%">
+    <div class="table-wrapper">
+      <TableItem class="salary-list" gtc="repeat(6, 1fr)" br="0.5rem">
+        <TableRow>
+          <TableCell th fs="1.7rem">지급 연월</TableCell>
+          <TableCell th fs="1.7rem">기본급</TableCell>
+          <TableCell th fs="1.7rem">비과세</TableCell>
+          <TableCell th fs="1.7rem">상여금</TableCell>
+          <TableCell th fs="1.7rem">공제 금액</TableCell>
+          <TableCell th fs="1.7rem">급여 총액</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell class="mid" fs="1.6rem"></TableCell>
+        </TableRow>
+      </TableItem>
+    </div>
+  </FlexItem>
 </template>
 
 <script setup>
-
+import FlexItem from "@/components/semantic/FlexItem.vue";
+import TableItem from "@/components/semantic/TableItem.vue";
+import TableRow from "@/components/semantic/TableRow.vue";
+import TableCell from "@/components/semantic/TableCell.vue";
 </script>
 
 <style scoped>
+.content-header {
+  position: relative;
+  justify-content: center;
+  align-items: center;
+}
+
+.content-body {
+  width: 80%;
+  margin-top: 2.5rem;
+}
 
 </style>
