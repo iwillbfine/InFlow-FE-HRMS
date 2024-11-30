cc593cad94db1a969c429aaafd6d8a46ec983070
Kibeom Jeon <kibeom1145@gmail.com>
2024-12-01 01:59:00 +0900

    Feat: 휴가 정책 페이지 구현

--- inflow/src/views/hr-management/vacation/VacationPolicyView.vue
+++ inflow/src/views/hr-management/vacation/VacationPolicyView.vue
@@ -1,21 +1,86 @@
 <template>
-  <CommonArticle label="휴가 정책 등록" w="90%">
-    <TableItem gtc="2.5fr 1.5fr 1fr 5fr 1.5fr 2fr">
+  <CommonArticle label="휴가 정책 등록" w="95%">
+    <TableItem gtc="1fr 2fr 1fr 2fr">
       <TableRow>
         <TableCell th fs="1.6rem" topl>휴가 정책명</TableCell>
-        <TableCell th fs="1.6rem">휴가 유형</TableCell>
-        <TableCell th fs="1.6rem">연도</TableCell>
-        <TableCell th fs="1.6rem">설명</TableCell>
-        <TableCell th fs="1.6rem">유급 휴가 여부</TableCell>
-        <TableCell th fs="1.6rem" topr>휴가 자동지급 주기</TableCell>
+        <TableCell fs="1.6rem">
+          <input
+            v-model="vacationPolicyName"
+            name="policy_name_input"
+            type="text"
+            placeholder="휴가 정책명을 입력해주세요."
+            maxlength="20"
+          />
+        </TableCell>
+        <TableCell th fs="1.6rem">휴가 정책 설명</TableCell>
+        <TableCell fs="1.6rem" topr>
+          <input
+            v-model="vacationPolicyDescription"
+            name="policy_description_input"
+            type="text"
+            placeholder="휴가에 대한 설명을 입력해주세요."
+            maxlength="30"
+          />
+        </TableCell>
       </TableRow>
       <TableRow>
-        <TableCell botl></TableCell>
-        <TableCell></TableCell>
-        <TableCell></TableCell>
-        <TableCell></TableCell>
-        <TableCell></TableCell>
-        <TableCell botr></TableCell>
+        <TableCell th fs="1.6rem">휴가 지급일 수</TableCell>
+        <TableCell fs="1.6rem">
+          <input
+            v-model="allocationDays"
+            name="allocation_days_input"
+            type="text"
+            placeholder="휴가 지급일 수를 입력해주세요."
+            maxlength="3"
+          />
+        </TableCell>
+        <TableCell th fs="1.6rem">휴가 유형</TableCell>
+        <TableCell class="h-7 pl-2" fs="1.6rem">
+          <DropdownItem
+            w="12rem"
+            :list="dropdownVacationTypeList"
+            placeholder="휴가 유형"
+            @update:selected-item="updateSelectedVacationType"
+          ></DropdownItem>
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell th fs="1.6rem">휴가 정책 적용 대상</TableCell>
+        <TableCell class="h-7 pl-2" fs="1.6rem">
+          <DropdownItem
+            w="16rem"
+            :list="vacationPolicyStatusList"
+            placeholder="정책 적용 대상"
+            @update:selected-item="updateSelectedVacationPolicyStatus"
+          ></DropdownItem>
+        </TableCell>
+        <TableCell th fs="1.6rem">휴가 정책 적용 연도</TableCell>
+        <TableCell class="h-7 pl-2" fs="1.6rem">
+          <YearDropDown
+            short
+            @valid-date-selected="updateSelectedYear"
+          ></YearDropDown>
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell th fs="1.6rem" botl>유급 휴가 여부</TableCell>
+        <TableCell class="h-7 pl-2" fs="1.6rem">
+          <DropdownItem
+            w="12rem"
+            :list="paidStatusList"
+            placeholder="유급/무급"
+            @update:selected-item="updateSelectedPaidStatus"
+          ></DropdownItem>
+        </TableCell>
+        <TableCell th fs="1.6rem">휴가 자동지급 주기</TableCell>
+        <TableCell class="h-7 pl-2" fs="1.6rem" botr>
+          <DropdownItem
+            w="12rem"
+            :list="autoAllocationCycleList"
+            placeholder="지급 주기"
+            @update:selected-item="updateSelectedAutoAllocationCycle"
+          ></DropdownItem>
+        </TableCell>
       </TableRow>
     </TableItem>
     <ButtonItem
@@ -27,19 +92,21 @@
       c="#fff"
       fs="1.6rem"
       @click="handleOnclick"
-      >신청</ButtonItem
+      >등록</ButtonItem
     >
   </CommonArticle>
   <hr />
-  <CommonArticle label="등록된 휴가 정책" w="90%">
-    <TableItem gtc="2.5fr 1.5fr 1fr 5fr 1.5fr 2fr">
+  <CommonArticle label="등록된 휴가 정책" w="95%">
+    <TableItem gtc="2.5fr 1fr 1fr 5fr 1.5fr 1fr 1fr 1fr">
       <TableRow>
-        <TableCell th fs="1.6rem">휴가 정책명</TableCell>
+        <TableCell th fs="1.6rem" topl>휴가 정책명</TableCell>
         <TableCell th fs="1.6rem">휴가 유형</TableCell>
         <TableCell th fs="1.6rem">연도</TableCell>
         <TableCell th fs="1.6rem">설명</TableCell>
-        <TableCell th fs="1.6rem">유급 휴가 여부</TableCell>
-        <TableCell th fs="1.6rem">휴가 자동지급 주기</TableCell>
+        <TableCell th fs="1.6rem">적용 대상</TableCell>
+        <TableCell th fs="1.6rem">지급일 수</TableCell>
+        <TableCell th fs="1.6rem">유/무급</TableCell>
+        <TableCell th fs="1.6rem" topr>지급 주기</TableCell>
       </TableRow>
       <TableRow
         v-for="(item, index) in vacationPolicyList"
@@ -59,13 +126,21 @@
         <TableCell class="mid" fs="1.6rem">{{
           item.vacation_policy_description
         }}</TableCell>
-        <TableCell class="mid" fs="1.6rem">{{ item.paid_status }}</TableCell>
+        <TableCell class="mid" fs="1.6rem">{{
+          parseVacationPolicyStatus(item.vacation_policy_status)
+        }}</TableCell>
+        <TableCell class="mid" fs="1.6rem">{{
+          item.allocation_days
+        }}</TableCell>
+        <TableCell class="mid" fs="1.6rem">{{
+          item.paid_status === 'Y' ? '유급' : '무급'
+        }}</TableCell>
         <TableCell
           class="mid"
           fs="1.6rem"
           :botr="index === vacationPolicyList.length - 1"
         >
-          {{ item.auto_allocation_cycle ? item.auto_allocation_cycle : '-' }}
+          {{ parseAutoAllocationCycle(item.auto_allocation_cycle) }}
         </TableCell>
       </TableRow>
     </TableItem>
@@ -77,9 +152,8 @@
       w="100%"
       fs="1.6rem"
     >
-      신청 내역이 존재하지 않습니다.
+      등록된 휴가 정책이 없습니다.
     </FlexItem>
-    <CrudModal v-if="isModalOpen" @close="toggleCancelRequestModal"></CrudModal>
   </CommonArticle>
 </template>
 
@@ -90,25 +164,83 @@ import TableRow from '@/components/semantic/TableRow.vue';
 import TableItem from '@/components/semantic/TableItem.vue';
 import FlexItem from '@/components/semantic/FlexItem.vue';
 import ButtonItem from '@/components/semantic/ButtonItem.vue';
-import CrudModal from '@/components/modals/CrudModal.vue';
+import DropdownItem from '@/components/dropdowns/DropdownItem.vue';
+import YearDropDown from '@/components/dropdowns/YearDropDown.vue';
 import { ref, onMounted } from 'vue';
 import {
+  getVacationTypes,
   getVacationPoliciesByYear,
-  createVacationRequest,
+  createVacationPolicy,
 } from '@/api/vacation';
 
 const eid = ref(null);
-const vacationList = ref([]);
+const vacationTypeList = ref([]);
 const vacationPolicyList = ref([]);
+const dropdownVacationTypeList = ref([]);
 const isEmpty = ref(true);
-const isModalOpen = ref(false);
 
-const arrayIndex = ref(null);
-const selectedVacation = ref(null);
-const selectedStartDate = ref('');
-const selectedEndDate = ref('');
-const requestReason = ref('');
-const fileList = ref([]);
+const vacationPolicyName = ref('');
+const vacationPolicyDescription = ref('');
+const allocationDays = ref(null);
+const selectedVacationPolicyStatus = ref('');
+const selectedVacationType = ref(null);
+const selectedYear = ref(null);
+const selectedPaidStatus = ref('');
+const selectedAutoAllocationCycle = ref('');
+
+const vacationPolicyStatusList = [
+  { id: 'NORMAL', name: '모든 사원' },
+  { id: 'WOMAN_ONLY', name: '여성 사원' },
+  { id: 'LONG_TERM', name: '장기 근속 사원' },
+  { id: 'ROOKIE', name: '신입 사원' },
+];
+
+const parseVacationPolicyStatus = (status) => {
+  switch (status) {
+    case 'NORMAL': return '모든 사원';
+    case 'WOMAN_ONLY': return '여성 사원';
+    case 'LONG_TERM': return '장기 근속 사원';
+    case 'ROOKIE': return '신입 사원';
+    default: return '모든사원';
+  }
+}
+
+const parseAutoAllocationCycle = (cycle) => {
+  switch (cycle) {
+    case '0 0 0 1 1 *': return '1년마다';
+    case '0 0 0 1 * *': return '1달마다';
+    default: return '-';
+  }
+}
+
+const paidStatusList = [
+  { id: 'Y', name: '유급' },
+  { id: 'N', name: '무급' },
+];
+
+const autoAllocationCycleList = [
+  { id: null, name: '선택 안함' },
+  { id: '0 0 0 1 1 *', name: '1년마다' },
+  { id: '0 0 0 1 * *', name: '1달마다' },
+];
+
+const fetchVacationTypeData = async () => {
+  const response = await getVacationTypes();
+
+  if (response.success) {
+    vacationTypeList.value = response.content;
+    for (const vacationType of vacationTypeList.value) {
+      dropdownVacationTypeList.value.push({
+        id: vacationType.vacation_type_id,
+        name: vacationType.vacation_type_name,
+      });
+    }
+    isEmpty.value = vacationTypeList.value.isEmpty ? true : false;
+  } else {
+    vacationTypeList.value = [];
+    isEmpty.value = true;
+  }
+};
 
 const fetchVacationPolicyData = async (year) => {
   const response = await getVacationPoliciesByYear(year);
@@ -122,116 +254,111 @@ const fetchVacationPolicyData = async (year) => {
   }
 };
 
-const toggleCancelRequestModal = () => {
-  isModalOpen.value = !isModalOpen.value;
+const updateSelectedVacationType = (id) => {
+  selectedVacationType.value = id;
 };
 
-// 일까지 파싱
-const parseDate = (dateStr) => {
-  const date = new Date(dateStr);
-
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1; // 월은 0부터 시작하므로 +1 필요
-  const day = date.getDate();
-
-  const formattedDate = `${year}년 ${month}월 ${day}일`;
-  return formattedDate;
+const updateSelectedYear = (year) => {
+  selectedYear.value = year;
 };
 
-const parseRequestStatus = (status) => {
-  switch (status) {
-    case 'ACCEPT':
-      return '승인됨';
-    case 'REJECT':
-      return '반려됨';
-    default:
-      return '대기중';
-  }
+const updateSelectedVacationPolicyStatus = (status) => {
+  selectedVacationPolicyStatus.value = status;
 };
 
-const checkValidDate = () => {
-  const selectedStart = new Date(selectedStartDate.value);
-  const selectedEnd = new Date(selectedEndDate.value);
+const updateSelectedPaidStatus = (status) => {
+  selectedPaidStatus.value = status;
+};
 
-  // 오늘 날짜를 Date 객체로 변환
-  const today = new Date();
-  today.setHours(0, 0, 0, 0); // 시간 비교를 위해 00:00:00으로 설정
-
-  if (selectedStart < today || selectedEnd < today) {
-    return false;
-  }
-
-  return true;
+const updateSelectedAutoAllocationCycle = (cycle) => {
+  selectedAutoAllocationCycle.value = cycle;
 };
 
 const handleOnclick = async () => {
-  if (!selectedVacation.value) {
-    alert('사용할 휴가를 선택하세요.');
+  if (!vacationPolicyName.value) {
+    alert('휴가 정책명을 입력해주세요.');
     return;
   }
 
-  if (!selectedStartDate.value) {
-    alert('휴가 시작일자를 선택하세요.');
+  if (vacationPolicyName.value.length > 20) {
+    alert('휴가 정책명을 20자 이내로 입력해주세요.');
     return;
   }
 
-  if (!selectedEndDate.value) {
-    alert('휴가 종료일자를 선택하세요.');
+  if (!vacationPolicyDescription.value) {
+    alert('휴가 정책에 대한 설명을 입력해주세요.');
     return;
   }
 
-  const startDate = new Date(selectedStartDate.value);
-  const endDate = new Date(selectedEndDate.value);
-  const durationDays = (endDate - startDate + 1) / (1000 * 60 * 60 * 24);
-
-  if (vacationList.value[arrayIndex.value].vacation_left < durationDays) {
-    alert('잔여 휴가 일수가 선택한 기간보다 적습니다.');
+  if (vacationPolicyDescription.value.length > 40) {
+    alert('휴가 정책에 대한 설명을 30자 이내로 입력해주세요.');
     return;
   }
 
-  if (!checkValidDate()) {
-    alert('휴가 날짜는 오늘보다 이전일 수 없습니다.');
+  if (!allocationDays.value) {
+    alert('휴가 지급일 수를 입력해주세요.');
     return;
   }
 
-  if (startDate > endDate) {
-    alert('휴가 종료일자는 휴가 시작일자보다 이전일 수 없습니다.');
+  const days = Number(allocationDays.value);
+  if (isNaN(days) || days <= 0) {
+    alert('휴가 지급일 수는 0보다 큰 숫자여야 합니다.');
     return;
   }
 
-  if (!requestReason.value) {
-    alert('휴가 사유를 입력하세요.');
+  if (!allocationDays.value) {
+    alert('휴가 지급일 수를 입력해주세요.');
     return;
   }
 
-  if (requestReason.value.length > 20) {
-    alert('신청 사유는 20자 이내로 작성해주세요.');
+  if (!selectedVacationType.value) {
+    alert('휴가 유형을 선택해주세요.');
     return;
   }
 
-  const formData = new FormData();
-  formData.append('request_reason', requestReason.value);
-  formData.append('start_date', selectedStartDate.value);
-  formData.append('end_date', selectedEndDate.value);
-  formData.append('employee_id', eid.value);
-  formData.append('vacation_id', selectedVacation.value);
+  if (!selectedVacationPolicyStatus.value) {
+    alert('정책 적용 대상을 선택해주세요.');
+    return;
+  }
 
-  fileList.value.forEach((file) => {
-    formData.append(`attachments`, file);
-  });
+  if (!selectedYear.value) {
+    alert('정책 적용 연도를 선택해주세요.');
+    return;
+  }
 
-  const response = await createVacationRequest(formData);
+  if (!selectedPaidStatus.value) {
+    alert('유급 휴가 여부를 선택해주세요.');
+    return;
+  }
 
-  selectedVacation.value = null; // 무한 요청 방지
-  selectedStartDate.value = ''; // 무한 요청 방지
-  selectedEndDate.value = ''; // 무한 요청 방지
-  requestReason.value = ''; // 무한 요청 방지
-  fileList.value = []; /// 무한 요청 방지
+  const formData = {
+    vacation_policy_name: vacationPolicyName.value,
+    vacation_policy_description: vacationPolicyDescription.value,
+    vacation_policy_status: selectedVacationPolicyStatus.value,
+    allocation_days: days,
+    paid_status: selectedPaidStatus.value,
+    year: selectedYear.value,
+    auto_allocation_cycle: selectedAutoAllocationCycle.value
+      ? selectedAutoAllocationCycle.value
+      : null,
+    vacation_type_id: selectedVacationType.value,
+    policy_register_id: Number(eid.value),
+  };
+
+  vacationPolicyName.value = '';
+  vacationPolicyDescription.value = '';
+  selectedVacationPolicyStatus.value = '';
+  allocationDays.value = null;
+  selectedPaidStatus.value = '';
+  selectedAutoAllocationCycle.value = null;
+  selectedVacationType.value = '';
+
+  const response = await createVacationPolicy(formData);
 
   if (response.success) {
-    alert('휴가 신청이 성공적으로 전송되었습니다.');
+    alert('휴가 정책이 성공적으로 등록되었습니다.');
   } else {
-    alert('휴가 신청 실패! 다시 시도해주세요.');
+    alert('휴가 정책 등록 실패! 다시 시도해주세요.');
   }
   window.location.reload();
 };
@@ -239,6 +366,7 @@ const handleOnclick = async () => {
 onMounted(() => {
   eid.value = localStorage.getItem('employeeId');
   const year = new Date().getFullYear();
+  fetchVacationTypeData();
   fetchVacationPolicyData(year);
 });
 </script>
@@ -255,7 +383,7 @@ hr {
   height: 7rem !important;
 }
 
-.pl-1 {
+.pl-2 {
   padding-left: 2rem;
 }
 
@@ -265,7 +393,12 @@ hr {
 
 .mid {
   justify-content: center;
-  align-items: center;
+}
+
+.tc input {
+  height: 100%;
+  width: 100%;
+  padding: 1rem;
 }
 
 .submit-btn {
