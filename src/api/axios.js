f13c99b9bb12fc6aea1e23c95785998d244caeb2
mingeun <caolian2003@naver.com>
2024-12-10 17:16:45 +0900

    Revert "임시커밋"
    This reverts commit 45aeb32fe6e5a20e01fbbaf57333ae826f371ffb.

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // Axios 인스턴스 생성
 const apiClient = axios.create({
-  // baseURL: 'https://api.inflow.run/api', // API 기본 경로
-  baseURL: 'http://localhost:5000/api', // API 기본 경로 ( 로컬 )
+  baseURL: 'https://api.inflow.run/api', // API 기본 경로
+  // baseURL: 'http://localhost:5000/api', // API 기본 경로 ( 로컬 )
 
 
 });
