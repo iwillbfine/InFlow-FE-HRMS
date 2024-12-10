37a57e3dcd7c762c59a6b963dcb4fbdad6a9a1ab
Kibeom Jeon <kibeom1145@gmail.com>
2024-12-10 22:52:11 +0900

    Refactor: 디자인 수정

--- src/components/employee-search/SearchBar.vue
+++ src/components/employee-search/SearchBar.vue
@@ -6,6 +6,7 @@
       placeholder="사원명/부서명/전화번호를 입력해주세요"
       @keyup.enter="onSearch"
       id="search-input-id"
+      maxlength="16"
     />
     <button class="search-button" @click="onSearch">검색</button>
   </div>
@@ -33,21 +34,24 @@ const onSearch = () => {
   height: 4rem;
   font-size: 2rem;
   font-weight: lighter;
+  justify-content: flex-end;
+  align-items: center;
+  background-color: #f8f8f8;
 }
 
 .search-input {
-  width: 95%;
-  background-color: #f8f8f8;
-  text-align: right;
-  padding: 0 21px;
+  width: calc(100% - 8.4rem);
+  text-align: start;
+  height: 100%;
+  padding-left: 136rem;
 }
 .search-input::placeholder {
-  text-align: right;
   font-size: 1.8rem;
 }
 
 .search-button {
-  width: 5%;
+  width: 8.4rem;
+  height: 100%;
   background-color: #003566;
   color: white;
 }
