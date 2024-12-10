45aeb32fe6e5a20e01fbbaf57333ae826f371ffb
mingeun <caolian2003@naver.com>
2024-12-10 17:06:57 +0900

    임시커밋

--- src/api/emp_info.js
+++ src/api/emp_info.js
@@ -8,8 +8,8 @@ import router from '@/router';
 export const login = async (formData) => {
   try {
     const response = await axios.post(
-      `https://api.inflow.run/api/login`,
-      // `http://localhost:5000/api/login`, ( 로컬 )
+      // `https://api.inflow.run/api/login`,
+      `http://localhost:5000/api/login`, // ( 로컬 )
       formData,
       {
         headers: {
