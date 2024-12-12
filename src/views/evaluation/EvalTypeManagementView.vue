6690d6545d9adc3f6c6252b663dd559ef1ab56c0
woodart8 <kibeom1145@gmail.com>
2024-12-12 15:58:31 +0900

    Refactor: 메뉴 이름 변경

--- src/views/evaluation/EvalTypeManagementView.vue
+++ src/views/evaluation/EvalTypeManagementView.vue
@@ -1,5 +1,5 @@
 <template>
-  <CommonArticle label="평가 유형 등록" w="90%">
+  <CommonArticle label="과제 유형 등록" w="90%">
     <TableItem gtc="2fr 0.3fr">
       <TableRow>
         <TableCell th fs="1.6rem">유형</TableCell>
@@ -36,7 +36,7 @@
 
   <hr />
 
-  <CommonArticle class="pos-rel" label="평가 유형 목록" w="90%">
+  <CommonArticle class="pos-rel" label="과제 유형 목록" w="90%">
     <TableItem gtc="0.25fr 2fr 0.25fr">
       <TableRow>
         <TableCell th fs="1.6rem">No</TableCell>
